2fac774bf8282a0e4ec50d0de5ad6fd6942a07df
plessiossauro <pedrolisckfilho@gmail.com>
2025-05-29 20:55:45 -0300

    Implementação de Funcionario - PN

--- src/controller/FuncionarioController.java
+++ src/controller/FuncionarioController.java
@@ -7,14 +7,34 @@ public class FuncionarioController {
 
     FuncionarioService vs = new FuncionarioService();
 
-    public void cadastraFuncionario(Funcionario funcionario){
+    public Funcionario cadastraFuncionario(String nome, String cpf, String cargo, String telefone) {
+        Funcionario funcionario = new Funcionario( nome,  cpf,  cargo,  telefone);
         vs.adcionarFuncionario(funcionario);
+        return funcionario;
 
     }
-    public void listaFuncionarios(){
+    public void listaFuncionarios(String nome){
         vs.mostrarFuncionario();
     }
-    public void deletarFuncionario(String cpf){
-        vs.removerFuncionario(cpf);
+
+    public Funcionario buscarPorNome(String nome) {
+        return vs.buscarPorNome(nome);
     }
+
+    public void deletarFuncionario(String nome){
+        vs.removerFuncionario(nome);
+    }
+
+
+    public boolean atualizarTodosDadosFuncionario(String novoTelefone, String novoNome, String novoCpf, String novoCargo){
+        return vs.atualizarTodosDadosFuncionario(novoTelefone, novoNome, novoCpf, novoCargo);
+    }
+
+    public void listarFuncionario() {
+        for (Funcionario funcionario : vs.listarFuncionario()) {
+            System.out.println(funcionario);
+        }
+    }
+
 }
+
